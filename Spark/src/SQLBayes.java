9432066786cb826817bff464b4027e306eb19760
madis <madis@madis-desktop>
2016-05-04 00:44:53 +0300

    Bayes also ready

--- Spark/src/SQLBayes.java
+++ Spark/src/SQLBayes.java
@@ -1,10 +1,6 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-import org.apache.log4j.Level;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -21,18 +17,13 @@ public class SQLBayes {
 	//http://nuncupatively.blogspot.com.ee/2011/07/naive-bayes-in-sql.html
 	public static void main(String[] args){
 		final long startTime = System.currentTimeMillis();
-		SparkConf conf = new SparkConf().setAppName("Naive Bayes in Spark SQL");
+		SparkConf conf = new SparkConf().setAppName("Naive Bayes in SparkSQL");
 		JavaSparkContext sc = new JavaSparkContext(conf);
-		List<Logger> loggers = Collections.<Logger>list(LogManager.getCurrentLoggers());
-		loggers.add(LogManager.getRootLogger());
-		for ( Logger logger : loggers ) {
-		    logger.setLevel(Level.ERROR);
-		}
 		HiveContext hc = new HiveContext(sc.sc());
 		hc.sql("SET	hive.metastore.warehouse.dir=file:///home/madis/workspace/SparkHiveSQL/tables");
 		
-		//JavaRDD<String> points = sc.textFile("input/converted.csv"); 
-		JavaRDD<String> points = sc.textFile("data/naiveBayesData.txt"); 
+		JavaRDD<String> points = sc.textFile(args[0],8); 
+		//JavaRDD<String> points = sc.textFile("data/naiveBayesData.txt"); 
 		String schemaString = "uid feature class"; //change here
 		List<StructField> fields = new ArrayList<StructField>();
 		for(String fieldName : schemaString.split(" ")){
@@ -41,6 +32,8 @@ public class SQLBayes {
 		StructType schema = DataTypes.createStructType(fields);
 		JavaRDD<Row> rowRDD = points.map(
 				new Function<String, Row>(){
+					private static final long serialVersionUID = 3282431521901483532L;
+
 					public Row call(String record) throws Exception {
 						String[] fields = record.split(",");
 						return RowFactory.create(fields[0], fields[1],fields[55]); //change here
@@ -51,7 +44,7 @@ public class SQLBayes {
 		pdf.registerTempTable("data");
 		
 
-		DataFrame a;
+		DataFrame a, b;
 		a = hc.sql("drop table agg");
 		a = hc.sql("drop table coefficients");
 		a = hc.sql("drop table scores");
@@ -59,30 +52,31 @@ public class SQLBayes {
 		//class is not null does not work for some reason, it was working but now it is not :( current workaround is to use A <> B
 		a = hc.sql("create table agg as "
 				+ "select feature, sum(1)+0.5 value, class from data group by feature, class");
-		a = hc.sql("select * from agg");
-		a.show();
+//		a = hc.sql("select * from agg");
+//		a.show();
 		
 		a = hc.sql("create table coefficients  as "
 				+ "select a.*, log(a.value/(b.value-a.value))-log(d.value/(t.tot-d.value)) coefficient from agg a "
 				+ "inner join (select feature, sum(value) as value from agg group by feature) b on a.feature = b.feature "
 				+ "inner join (select class, sum(value) as value from agg group by class) d on a.class=d.class "
 				+ "inner join (select sum(value) tot from agg) t on 1 = 1");
-		a = hc.sql("select * from coefficients");
-		a.show();
+//		a = hc.sql("select * from coefficients");
+//		a.show();
 		
 		a = hc.sql("create table scores  as "
 				+ "select a.uid, a.class as actual, b.class as prediction, sum(coefficient) score from data a "
 				+ "inner join coefficients b on a.feature = b.feature "
 				+ "group by a.uid, a.class, b.class");
-		a = hc.sql("select * from scores");
-		a.show();
+//		a = hc.sql("select * from scores");
+//		a.show();
 		
-		a = hc.sql("select a.uid, actual, prediction, score from scores a "
+		b = hc.sql("select a.uid, actual, prediction, score from scores a "
 				+ "inner join (select c.uid, max(score) maxscore from scores c group by c.uid) b on a.uid = b.uid and a.score = maxscore");
-		a.show();
+//		b.show(100);
 		
 		final long endTime = System.currentTimeMillis();
 		System.out.println("Execution time: " + (endTime - startTime) );
+		b.rdd().saveAsTextFile(args[0]+String.valueOf(endTime) +"SQL bayes out" + String.valueOf(rowRDD.count()));
 		sc.close();
 	}
 }
--- Spark/src/SparkBayes.java
+++ Spark/src/SparkBayes.java
@@ -2,71 +2,97 @@
 //https://github.com/apache/spark/blob/master/examples/src/main/java/org/apache/spark/examples/mllib/JavaNaiveBayesExample.java
 //https://github.com/apache/spark/commit/2804674a7af8f11eeb1280459bc9145815398eed
 // $example on$
-import scala.Tuple2;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.PairFunction;
+import java.util.ArrayList;
+import java.util.List;
+
+// $example off$
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaDoubleRDD;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.mllib.classification.NaiveBayes;
 import org.apache.spark.mllib.classification.NaiveBayesModel;
+import org.apache.spark.mllib.linalg.Vectors;
 import org.apache.spark.mllib.regression.LabeledPoint;
-import org.apache.spark.mllib.util.MLUtils;
 
-import java.util.Collections;
-import java.util.List;
-
-import org.apache.log4j.Level;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-// $example off$
-import org.apache.spark.SparkConf;
-import org.apache.spark.SparkContext;
+import scala.Tuple2;
 
 public class SparkBayes {
 	public static void main(String[] args) {
 		final long startTime = System.currentTimeMillis();
 		SparkConf sparkConf = new SparkConf().setAppName("JavaNaiveBayesExample");
 		JavaSparkContext jsc = new JavaSparkContext(sparkConf);
-		SparkContext sc = jsc.sc();
-		//c) If you skip one in between, it should be assigned a default value of zero.
-		//In short, +1 1:0.7 2:1 3:1 translates to:
-		//Assign to class +1, the point (0.7,1,1).
-		String path = "data/bayes spark3.txt";
-		JavaRDD<LabeledPoint> inputData = MLUtils.loadLibSVMFile(jsc.sc(), path).toJavaRDD();
-		JavaRDD<LabeledPoint>[] tmp = inputData.randomSplit(new double[]{0.6, 0.4}, 12345);
+		// c) If you skip one in between, it should be assigned a default value
+		// of zero.
+		// In short, +1 1:0.7 2:1 3:1 translates to:
+		// Assign to class +1, the point (0.7,1,1).
+		JavaRDD<String> data = jsc.textFile(args[0],8);
+		JavaRDD<LabeledPoint> inputData = data.map(new Function<String, LabeledPoint>() {
+			private static final long serialVersionUID = 7004546027616594543L;
+
+			public LabeledPoint call(String line) {
+				String[] parts = line.split(",");
+				double[] v = new double[8];
+				v[0] = Double.parseDouble(parts[1]);
+				v[1] = Double.parseDouble(parts[2]);
+				v[2] = Double.parseDouble(parts[3]);
+				v[3] = Double.parseDouble(parts[4]);
+				v[4] = Double.parseDouble(parts[6]);
+				v[5] = Double.parseDouble(parts[7]);
+				v[6] = Double.parseDouble(parts[8]);
+				v[7] = Double.parseDouble(parts[9]);
+				return new LabeledPoint(Double.parseDouble(parts[55]), Vectors.dense(v));
+				
+			}
+		});
+		inputData.cache();
+		//String path = "data/bayes spark3.txt";
+//		JavaRDD<LabeledPoint> inputData = MLUtils.loadLibSVMFile(jsc.sc(), path).toJavaRDD();
+		JavaRDD<LabeledPoint>[] tmp = inputData.randomSplit(new double[] { 0.8, 0.2 }, 12345);
 		JavaRDD<LabeledPoint> training = tmp[0]; // training set
 		JavaRDD<LabeledPoint> test = tmp[1]; // test set
 		final NaiveBayesModel model = NaiveBayes.train(training.rdd(), 1.0);
-		JavaPairRDD<Double, Double> predictionAndLabel =
-		  test.mapToPair(new PairFunction<LabeledPoint, Double, Double>() {
-		    /**
-			 * 
-			 */
-			private static final long serialVersionUID = 5970068786622801541L;
+		JavaPairRDD<Double, Double> predictionAndLabel = test
+				.mapToPair(new PairFunction<LabeledPoint, Double, Double>() {
+					private static final long serialVersionUID = 5970068786622801541L;
 
-			@Override
-		    public Tuple2<Double, Double> call(LabeledPoint p) {
-		    	System.out.println(p.features()+ " actual " + p.label() + " prediction " + model.predict(p.features()));
-		      return new Tuple2<Double, Double>(model.predict(p.features()), p.label());
-		    }
-		  });
+					@Override
+					public Tuple2<Double, Double> call(LabeledPoint p) {
+//						System.out.println(p.features() + " actual " + p.label() + " prediction " + model.predict(p.features()));
+						return new Tuple2<Double, Double>(model.predict(p.features()), p.label());
+					}
+				});
+		
+		
 		double accuracy = predictionAndLabel.filter(new Function<Tuple2<Double, Double>, Boolean>() {
 			private static final long serialVersionUID = -7042070591367537117L;
 
-		@Override
-		  public Boolean call(Tuple2<Double, Double> pl) {
-		    return pl._1().equals(pl._2());
-		  }
+			@Override
+			public Boolean call(Tuple2<Double, Double> pl) {
+				return pl._1().equals(pl._2());
+			}
 		}).count() / (double) test.count();
-		System.out.println(accuracy);
+		
+		
+		
+		
+		System.out.println("training size " + training.count() + " test size " + test.count());
+		System.out.println("accuracy " + accuracy);
 		System.out.println();
-		for(double el : model.pi()){
+		List<Double> answer = new ArrayList<Double>();
+		for (double el : model.pi()) {
+			answer.add(el);
 			System.out.println(el);
 		}
 		// Save and load model
-//		model.save(jsc.sc(), "target/tmp/myNaiveBayesModel");
+		// model.save(jsc.sc(), "target/tmp/myNaiveBayesModel");
 		final long endTime = System.currentTimeMillis();
-		System.out.println("Total execution time: " + (endTime - startTime) );
+		JavaDoubleRDD out = jsc.parallelizeDoubles(answer);
+		out.saveAsTextFile(args[0] + String.valueOf(endTime) +"Spark Bayes Out " +String.valueOf(inputData.count()));
+		System.out.println("Total execution time: " + (endTime - startTime));
+		jsc.close();
 	}
 }
